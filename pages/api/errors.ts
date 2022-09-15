01d45f72a1660169096ce1bbd826d1b001f5dea8
Løvgren, Dag Erik <dag.erik.lovgren@miles.no>
2022-09-15 06:00:58 +0200

    Add support for sorting and pagination to errors endpoint

--- pages/api/errors.ts
+++ pages/api/errors.ts
@@ -13,9 +13,42 @@ const handler = async (req: NextApiRequest, res: NextApiResponse<IError[]>) => {
     return;
   }
 
-  res
-    .status(200)
-    .json(require("../../src/_workshop-internals/data/errors.json"));
+  let result: IError[] = require("../../src/_workshop-internals/data/errors.json");
+
+  if ("sort" in req.query) {
+    result = result.sort((a, b) => {
+      switch (req.query.sort) {
+        case "timestamp":
+          return b.timestamp - a.timestamp;
+        case "severity":
+          return b.severity - a.severity;
+        case "message":
+          if (!a.message && !b.message) {
+            return 0;
+          } else if (!a.message) {
+            return 1;
+          } else if (!b.message) {
+            return -1;
+          }
+
+          return a.message.localeCompare(b.message);
+      }
+
+      return 0;
+    });
+  }
+
+  if ("index" in req.query && "limit" in req.query) {
+    const index = +req.query.index!;
+    const limit = +req.query.limit!;
+
+    const start = index * limit;
+    const end = start + limit;
+
+    result = result.slice(start, end);
+  }
+
+  res.status(200).json(result);
 };
 
 export default handler;
